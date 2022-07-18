cfe77de220fbb724bcd964305ccf8e6038ce136e
Guada Garcilazo <guadalupe.garcilazo@arbusta.net>
2022-07-18 15:53:25 -0300

    Ejercicio App Twitter

---
+++ Twitter/holamundo/src/NombreUsuario.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+ class Nombre extends React.Component {
+    render(){
+        return (
+            <div>
+                 <h2> Manuel Gutierrez <span>@ManuelGutierrez</span></h2>
+            </div>
+        )
+     }
+}
+
+export default Nombre;
